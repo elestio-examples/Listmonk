9c2e693103d7599329b3fce7098043e3d8a6cfd1
Prem chavhan <pchavhan@logiquad.com>
2023-12-07 14:09:03 +0530

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,8 +1,2 @@
-cd /listmonk
-export GO111MODULE=on
-GOBIN=$(pwd)/bin
-export GOBIN
-go build -o bin/listmonk ./cmd/listmonk
-cd /../
-cd listmonk && make dist #binary will be generated
+make dist #binary will be generated
 docker buildx build . --output type=docker,name=elestio4test/listmonk:latest | docker load
